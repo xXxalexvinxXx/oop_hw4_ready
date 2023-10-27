2c6bd15f82b97098ad878ae4d52c5d96f6462da9
Al Vin <zdejh.gm@gmail.com>
2023-10-27 14:32:21 +0300

    add edit teacher

--- TeacherService.java
+++ TeacherService.java
@@ -17,6 +17,12 @@ public class TeacherService implements UserService<Teacher> {
         Teacher teacher = new Teacher(firstName, secondName,lastName,++maxId);
         teachers.add(teacher);
     }
+    public void edit(int Id, String firstName, String secondName, String lastName){
+        Teacher teacher = teachers.get(Id);
+        teacher.setFirstName(firstName);
+        teacher.setSecondName(secondName);
+        teacher.setLastName(lastName);
+    }
 
     @Override
     public List<Teacher> getAll() {
